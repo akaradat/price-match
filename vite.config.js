9c56594dea0812d7b4dcd5593c7aef1a33eb7115
AKRD KMNC <akrd.kmnc@gmail.com>
2024-08-21 00:54:32 +0700

    build: add github action

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          VITE_WEBSITE_URL: process.env.VITE_WEBSITE_URL,
+        },
+      },
+    }),
+  ],
+  base: process.env.VITE_BASE_URL || '/',
+});
